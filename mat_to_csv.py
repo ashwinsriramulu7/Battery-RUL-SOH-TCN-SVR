eb077a0c35f5bf329c814e6883bc08cf7be7260f
ashwinsriramulu7 <ashwinsriramulu7@gmail.com>
2025-11-24 19:42:02 +0530

    Added Raw dataset under raw-data directory

---
+++ mat_to_csv.py
@@ -0,0 +1,6 @@
+"""
+The dataset used in this project is the NASA Battery Dataset, created and made available \
+online by the NASA Ames Research Center. It consists of charge/discharge data for different \
+batteries, in .mat format. The first step is to convert the .mat files to \
+.csv file format for Exploratory Data Analysis and Downstream processing before actually training ML models. 
+"""
