737e51fb80a8cda83188c5bc745c1a61fef0b325
ashwinsriramulu7 <ashwinsriramulu7@gmail.com>
2025-11-24 19:54:25 +0530

    imports: added all required imports for .mat to .csv conversion

--- mat_to_csv.py
+++ mat_to_csv.py
@@ -4,3 +4,8 @@ online by the NASA Ames Research Center. It consists of charge/discharge data fo
 batteries, in .mat format. The first step is to convert the .mat files to \
 .csv file format for Exploratory Data Analysis and Downstream processing before actually training ML models. 
 """
+
+import scipy.io as sio # for reading the .mat data
+import pandas as pd # to create and manipulate the dataframes
+import numpy as np # for reading, creating and manipulating arrays
+from datetime import datetime # to add proper formatted timestamps for records
