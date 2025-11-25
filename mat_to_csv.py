e1e014b102a89ab9b10d9a9001b24321286308f2
ashwinsriramulu7 <ashwinsriramulu7@gmail.com>
2025-11-25 11:46:52 +0530

    Feat: Added a utility function | Accepts: Matlab Date Vector | Returns: Python datetime object

--- mat_to_csv.py
+++ mat_to_csv.py
@@ -9,3 +9,21 @@ import scipy.io as sio # for reading the .mat data
 import pandas as pd # to create and manipulate the dataframes
 import numpy as np # for reading, creating and manipulating arrays
 from datetime import datetime # to add proper formatted timestamps for records
+
+## <-> 
+# Utility functions 
+## <-> 
+
+##Util001 - convert matlab datevec to python datetime
+def matlab_dv_to_pyth_dt(vec):
+    """
+    Convert MATLAB datevec [Y M D h m s] to Python datetime format
+    """
+    if vec is None: #empty datevec 
+        return None
+    try:
+        Y, M, D, h, m, s = vec 
+        return datetime(int(y), int(m), int(d), int(H), int(M), int(S))
+    except:
+        return None
+
